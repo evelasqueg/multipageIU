2656afadbdeb4c717528930071878356c8629f57
evelasqueg <165836786+evelasqueg@users.noreply.github.com>
2024-04-23 13:53:09 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -7,6 +7,5 @@ st.title("Inteligencia Urbana: Introducción")
 image = Image.open('Inteligencia_Urbana.jpeg')
 st.image(image)
 
-st.write("La Inteligencia Urbana “Es una inteligencia repartida en todas partes, valorizada constantemente, 
-coordinada en tiempo real, que conduce a una movilización efectiva de las competencias.” (Lévy, 2004, p. 19)")
+st.write("La Inteligencia Urbana “Es una inteligencia repartida en todas partes, valorizada constantemente, coordinada en tiempo real, que conduce a una movilización efectiva de las competencias.” (Lévy, 2004, p. 19)")
 
