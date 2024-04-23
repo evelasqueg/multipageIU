8c3c2bbce161b3a6eeaf944f8257a814e8a57869
evelasqueg <165836786+evelasqueg@users.noreply.github.com>
2024-04-23 13:58:09 -0500

    Update AD.py

--- pages/AD.py
+++ pages/AD.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
 st.title ("Analítica de Datos")
+
+uploaded_file = st.file_uploader('Choose a file')
+if uploaded_file is not None:
+   df1=pd.read_csv(uploaded_file)
+   st.write(df1)
+   st.subheader('Estadísticos básicos de los sensores')
+   st.dataframe(df1["temperatura ESP32"].describe())
+else:
+ st.warning('Necesitas cargar un archivo csv excel.')
