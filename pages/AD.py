04a1bfa2b74b9af432cec0000205a8d8fe1ab068
evelasqueg <165836786+evelasqueg@users.noreply.github.com>
2024-04-23 13:59:37 -0500

    Update AD.py

--- pages/AD.py
+++ pages/AD.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import geopandas as gpd
+import json
+
 
 st.title ("Analítica de Datos")
 
@@ -10,3 +15,21 @@ if uploaded_file is not None:
    st.dataframe(df1["temperatura ESP32"].describe())
 else:
  st.warning('Necesitas cargar un archivo csv excel.')
+
+
+with open('redes_camineras.geojson', "r") as read_file:
+    data = json.load(read_file)
+
+st.title("Redes Camineras Medellín 2022")
+
+st.write('Las redes camineras son espacios públicos seguros para el peatón, logrados a través de urbanismo táctico, que además pretenden la organización de los flujos viales, reducir los conflictos entre actores viales (peatón, moto, vehículo, transporte público, entre otros), acortar la distancia para cruzar la vía, eliminar el estacionamiento irregular y mejorar la accesibilidad para personas con movilidad reducida.)'
+         )
+st.subheader('Sistema de consulta de redes camineras de Medellín')
+
+longitud_km = data['features'][0]['properties']['LONGITUD_KM']
+estado = data['features'][0]['properties']['ESTADO']
+nombre = data['features'][0]['properties']['NOMBRE']
+
+print("Longitud en kilómetros:", longitud_km)
+print("Estado:", estado)
+print("Nombre:", nombre)
