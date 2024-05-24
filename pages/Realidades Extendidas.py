784475a50ad7722d91c9410d81b2cd24af6e6394
evelasqueg <165836786+evelasqueg@users.noreply.github.com>
2024-05-24 08:39:43 -0500

    Update Realidades Extendidas.py

--- pages/Realidades Extendidas.py
+++ pages/Realidades Extendidas.py
@@ -1,1 +1,7 @@
+st.title("Realidades Extendidas")
 
+st.write("Las realidades extendidas son tecnologías inmersivas que unen elementos virtuales y reales en una interacción máquina - humano. Estas interacciones pueden revolucionar la forma en la que trabajamos, estudiamos y vivimos los espacios.")
+
+st.write("Poder experimentar la ciudad desde las realidades extendidas, permitirían dar acceso a lugares e información sin tener que salir de casa, permitiendo llegar a lugar alejados y disminuyendo los tiempos de desplazamiento.")
+
+st.write("En el siguiente enlace, podrás experimentar una noche de fogata, donde hablaremos sobre Analítica de Datos, Inteligencia artificial y el internet de las cosas: ")
