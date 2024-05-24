606d2333178fe9a4ad54f08ffc3ab8616a1bed7a
evelasqueg <165836786+evelasqueg@users.noreply.github.com>
2024-05-24 08:30:24 -0500

    Update Iot.py

--- pages/Iot.py
+++ pages/Iot.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.title ("Internet de las cosas")
+
+st.write("El internet de las cosas se refiere a la interconexión de dispositivos físicos que están integrados con sensores, software y conectividad para recopilar y compartir datos entre sí y con otras plataformas")
+
+st.write("Para las ciudades, la aplicación de herramientas de IoT aporta a la solución de problemáticas urbanas y el monitoreo de dinámicas claves para la ciudad")
+
+st.write("En el siguiente podrás monitorear la humedad y temperatura de la ciudad para definir como estoas variables inciden en el trafico urbano. Ingresa a https://demo.thingsboard.io/dashboards/0a666910-121d-11ef-b68a-bfe4060367e5")
